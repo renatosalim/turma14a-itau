1f057aada8affeb0f3d5150fbefdb79b41496f35
renatosalim <resalim@gmail.com>
2021-02-04 15:23:43 -0300

    colocando um quadrado no html

--- index.php
+++ index.php
@@ -5,13 +5,12 @@
 <h5>Tentar sempre, desistir jamais!!!<h5>
 
 <div style="display:flex">
-<div style="witdh:300px;height:300px;background:lightblue;color:red;color:black;padding:10px;margin:50px;">
+<div style="witdh:300px;height:300px;background:lightblue;color:black;padding:10px;margin:50px;">
     Cloud
    <br>  
    <img src= "cloud.jfif" style = witdh:50px">
 </div>
-<div 
-style="witdh:300px;height:300px;background:lightgreen;color:red;color:black;padding:10px;margin:50px;">
+<div style="witdh:300px;height:300px;background:lightgreen;color:black;padding:10px;margin:50px;">
     Internet das coisas
    <br> 
    <img src= "IOT.jfif" style = witdh:50px">
