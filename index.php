f7c903d0fbc76d69705c997e0ce6566e001250b0
renatosalim <resalim@gmail.com>
2021-02-04 14:13:45 -0300

    colocando um quadrado no html

--- index.php
+++ index.php
@@ -3,3 +3,7 @@
 <h3> Um deploy aotomático feito com alunos do itaú </h3>
 
 <h5>Tentar sempre, desistir jamais!!!<h5>
+
+<div style="witdh:200px;height:200px;background:yellow">
+    Um teste em um quadrado
+<div>
